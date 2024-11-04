305102a4907f08db913c5c7a70875d009523ef35
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-11-04 17:07:24 -0500

    some code added to script.js file and index.html modified

---
+++ flixx-app/js/script.js
@@ -0,0 +1,72 @@
+const global = {
+    currentPage: window.location.pathname,
+};
+
+async function displayPopularMovies() {
+    const { results } = await fetchAPIData('movie/popular');
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+          <a href="movie-details.html?id=1">
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Movie Title</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: XX/XX/XXXX</small>
+            </p>
+          </div>`;
+    });
+}
+// Highlight active link
+function highlightActiveLink() {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach((link) => {
+        if (link.getAttribute('href') === global.currentPage) {
+            link.classList.add('active');
+        }
+    });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+    const API_KEY = 'f1d3c95b2c0cbdf21b1e06a9b83617a9';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+    const data = await response.json();
+
+    return data;
+}
+
+// Init App
+function init() {
+    switch (global.currentPage) {
+        case '/':
+        case '/index.html':
+            displayPopularMovies();
+            break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+    }
+
+    highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
