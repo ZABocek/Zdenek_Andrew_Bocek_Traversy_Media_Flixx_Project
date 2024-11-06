1b5110ca95584df97d1f586c0a7968d53c21adef
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-11-06 17:08:51 -0500

    modified script.js

--- flixx-app/js/script.js
+++ flixx-app/js/script.js
@@ -72,6 +72,10 @@ async function displayPopularShows() {
 async function displayMovieDetails() {
     const movieId = window.location.search.split('=')[1];
     const movie = await fetchAPIData(`movie/${movieId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
     div.innerHTML = `<div class="details-top">
           <div>
@@ -118,6 +122,28 @@ async function displayMovieDetails() {
           <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
         </div>`;
     document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display Backdrop On Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
 }
 
 function showSpinner() {
