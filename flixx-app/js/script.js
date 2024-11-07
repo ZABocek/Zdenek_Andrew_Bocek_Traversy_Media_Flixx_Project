08face6e7f76c8857e539ec6858efabf178320df
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-11-07 11:03:14 -0500

    modified script.js file

--- flixx-app/js/script.js
+++ flixx-app/js/script.js
@@ -133,49 +133,55 @@ async function displayShowDetails() {
     displayBackgroundImage('tv', show.backdrop_path);
 
     const div = document.createElement('div');
-    div.innerHTML = `<div class="details-top">
-          <div>
+    div.innerHTML = `
+    <div class="details-top">
+        <div>
             ${
-                    show.poster_path
-                        ? `<img
-                    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                    class="card-img-top"
-                    alt="${show.name}"
-                />`
-                        : `<img
-                    src="images/no-image.jpg"
-                    class="card-img-top"
-                    alt="${show.name}
-                />`
+                show.poster_path
+                    ? `<img
+                        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                        class="card-img-top"
+                        alt="${show.name}"
+                    />`
+                    : `<img
+                        src="images/no-image.jpg"
+                        class="card-img-top"
+                        alt="${show.name}"
+                    />`
             }
-          </div>
-          <div>
+        </div>
+        <div>
             <h2>${show.name}</h2>
             <p>
-              <i class="fas fa-star text-primary"></i>
-              ${show.vote_average.toFixed(1)} / 10
+                <i class="fas fa-star text-primary"></i>
+                ${show.vote_average.toFixed(1)} / 10
             </p>
             <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
             <p>
-              ${show.overview}
+                ${show.overview}
             </p>
             <h5>Genres</h5>
             <ul class="list-group">
-              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+                ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
             </ul>
             <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
-          </div>
         </div>
-        <div class="details-bottom">
-          <h2>Show Info</h2>
-          <ul>
-            <li><span class="text-secondary">Number of Episodes: </span> ${show.number_of_episodes}</li>
-            <li><span class="text-secondary">Last Episode to Air: </span> ${show.last_episode_to_air}</li>
-            <li><span class="text-secondary">Status: </span> ${show.status}</li>
-          </ul>
-          <h4>Production Companies</h4>
-          <div class="list-group">${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
-        </div>`;
+    </div>
+    <div class="details-bottom">
+        <h2>Show Info</h2>
+        <ul>
+            <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
+            <li>
+                <span class="text-secondary">Last Episode to Air:</span>
+                ${show.last_episode_to_air ? show.last_episode_to_air.name : 'N/A'}
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">
+            ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+        </div>
+    </div>`;
     document.querySelector('#show-details').appendChild(div);
 }
 
